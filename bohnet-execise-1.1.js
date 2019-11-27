8f6033c59249d9328d46ed5dddcd891da4c1922f
Christine Bohnet <christine.bohnet@gmail.com>
2019-11-26 20:38:49 -0600

    2 error exercise

---
+++ bohnet-execise-1.1.js
@@ -0,0 +1,20 @@
+/*
+============================================
+; Title: Exercise 1.1
+; Author: Christine Bohnet
+; Date: 26 Nov 2019
+; Modified By: <Your name>
+; Description: This program demonstrates the
+; use of errors in an application
+;===========================================
+*/
+
+var x = 5;
+var y = "6";
+var z = null;
+
+alert("x + y = " + (x + y));
+alert("x / z = " + (x / z));
+
+
+
