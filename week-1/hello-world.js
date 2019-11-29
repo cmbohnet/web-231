d0b6c7b96a1c9bbe502011df610d122d3b519dd5
Christine Bohnet <christine.bohnet@gmail.com>
2019-11-29 11:07:31 -0600

    hello-world week 1

--- week-1/hello-world.js
+++ week-1/hello-world.js
@@ -1,1 +1,11 @@
+/*
+============================================
+; Title: Exercise 1.3
+; Author: Christine Bohnet
+; Date: 29 Nov 2019
+; Modified By: Christine Bohnet
+; Description: This program demonstrates the
+; use of console.log and debug in an application
+;===========================================
+*/
 console.log("hello world, Christine Bohnet");
