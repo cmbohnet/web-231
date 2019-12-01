b99cfcd2713d199e69cd99f8eb5229136d4ed6ca
Christine Bohnet <christine.bohnet@gmail.com>
2019-12-01 11:24:43 -0600

    bohnet-assignment-1.4

---
+++ week-1/bohnet-assignment-1.4.js
@@ -0,0 +1,22 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Author: Christine Bohnet
+; Date: 29 Nov 2019
+; Modified By: Christine Bohnet
+; Description: This program demonstrates the
+; use of primitive variables in an application
+;===========================================
+*/
+
+var firstName = "Christine";
+var lastName = "Bohnet";
+var age = "29";
+var street = "Augusta Ave";
+var city = "Omaha";
+var zip = "68123";
+
+console.log("First Last: ", firstName, lastName);
+console.log("Age: ", age);
+console.log("Street: ", street);
+console.log("City, Zip: ", city, zip);
