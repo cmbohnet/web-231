a95c28235f4397ad2302d69ca9e3570cc0c5409a
Christine Bohnet <christine.bohnet@gmail.com>
2019-12-01 20:04:59 -0600

    Corrected errors

---
+++ week-1/SolutionKinney.js
@@ -0,0 +1,20 @@
+/*=================================================; 
+Title: Assignment 1.1; 
+Author: Chris Bohnet - Corrections to Sean Kinney's code; 
+Date: 1 December 2019; 
+Modified by: Chris Bohnet; 
+Description: This program does not work.  Corrected two errors that; must be fixed=================================================*/
+// variable that is assigned today's date
+
+var todaysDate = new Date();
+// variable that is assigned a past year
+
+var pastYear = 1776;
+// variable
+//Display the current date
+
+console.log("Today's date and time is " + todaysDate);
+//Display a past year value
+
+console.log(pastYear + " occurred in the past");
+
