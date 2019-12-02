8464edf9d7eaf4d885f8c32dda7e60bac748209f
Christine Bohnet <christine.bohnet@gmail.com>
2019-12-01 20:17:36 -0600

    Corrected errors

---
+++ week-1/SolutionKornelis.js
@@ -0,0 +1,26 @@
+/*
+============================================
+; Title: Assignment 1.1
+; Author: Izabella Kornelis
+; Date: 27 November 2019
+; Modified By: Izabella Kornelis
+; Description: A simple JavaScript program using values and variables with at least two errors.
+;===========================================
+*/
+
+//program start
+
+//variable declaration
+var x = 10;
+
+//creating function
+function multiply(num) {
+    var product = 0;
+    product = num*num;
+    return product;
+};
+
+//output
+console.log(multiply(x));
+
+//end program
