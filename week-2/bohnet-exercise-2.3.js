cc1ede48c2ae1ca687877d03cce45fc24462ed4a
Christine Bohnet <christine.bohnet@gmail.com>
2019-12-05 21:17:04 -0600

    week 2 exercise-2.3

---
+++ week-2/bohnet-exercise-2.3.js
@@ -0,0 +1,8 @@
+const header = require('../bohnet-header.js');
+console.log(header.display('Christine', 'Bohnet', 'week-2-exercise-2.3'))
+
+function myName(){
+    myName.christine = 'Christine';
+    return myName.christine;
+}
+console.log(myName());
