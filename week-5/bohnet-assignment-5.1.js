3da679170ac0ef90581a61b6757c22fcea67800c
Christine Bohnet <christine.bohnet@gmail.com>
2020-01-09 17:25:41 -0600

    2 errors

---
+++ week-5/bohnet-assignment-5.1.js
@@ -0,0 +1,20 @@
+
+const header = require('../bohnet-header.js');
+console.log(header.display('Christine', 'Bohnet', 'bohnet-assignment-5.1'))
+
+
+
+
+var sayings = new Map();
+sayings.set('dog', 'woof');
+sayings.set('cat', 'meow');
+sayings.set('elephant', 'toot');
+sayings.size; // 3
+sayings.get('fox'); // undefined
+sayings.has('bird'); // false
+sayings.delete('dog');
+sayings.has('dog'); // false
+
+for (var [key; value] of saying) {
+  console.log(key + ' goes ' + value);
+}
